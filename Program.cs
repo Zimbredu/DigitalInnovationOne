1dcac91ca25fad5337b4018f74c0b734f915ee50
Eduardo Antonio Zimbre <edurdo_zimbre@yahoo.com.br>
2021-02-10 22:20:29 -0300

    Aprendendo C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int numeroDevezes = 5;
+
+           for(int i = 0; i < numeroDevezes; i++)
+           {
+               Console.WriteLine($"Bem-vindo ao curso de .Net {i}");
+           }         
+
+        }
+    }
+}
